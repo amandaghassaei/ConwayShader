12ed31f6286ff09a9de4c503260371d4a4d17772
Amanda Ghassaei <amandaghassaei@gmail.com>
2020-11-07 21:56:53 -0800

    updates to gpu lib

--- src/kernels/InteractionShader.glsl
+++ src/kernels/InteractionShader.glsl
@@ -1,9 +1,9 @@
 precision lowp float;
 
-varying vec2 uv;
+varying vec2 uv_local;
 uniform sampler2D u_noiseLookup;
 
 void main() {
-	float state = texture2D(u_noiseLookup, uv).r;
+	float state = texture2D(u_noiseLookup, uv_local).r;
 	gl_FragColor = vec4(state, 0, 0, 1);
 }
